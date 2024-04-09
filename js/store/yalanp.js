311107fc2eebdb0f1c289a08746ef3ce97070c42
fathima-Sara-Shums <cb014190@students.apiit.lk>
2024-04-09 14:07:16 +0530

    content made dynamic

--- js/store/yalanp.js
+++ js/store/yalanp.js
@@ -4,24 +4,15 @@ document.addEventListener('alpine:init', () => {
         
         main:{
             title:"Yala National Park",
-            body:"Yala (යාල/யாலா தேசிய பூங்கா) National Park is a mesmerizing haven for wildlife enthusiasts, situated in the southeastern region of Sri Lanka, bordering the Indian Ocean. Covering an expansive 979 square kilometers, Yala is the most visited and second-largest national park in the country.The park comprises five distinct blocks, with three open to the public. Notable among these are Palatupana (Block 1), Kumana National Park, also known as 'Yala East,' and Lunugamvehera National Park. Its strategic location, approximately 300 kilometers from Colombo, makes it easily accessible for nature lovers. Designated as a wildlife sanctuary in 1900, alongside Wilpattu, Yala has been a pioneer in wildlife conservation in Sri Lanka. Renowned for its incredible biodiversity, the park is a crucial habitat for Sri Lankan elephants, Sri Lankan leopards, and a plethora of aquatic bird species ,Explore the diverse landscapes within Yala National Park, from lush forests to arid plains, and witness the coexistence of a wide array of flora and fauna. The park's significance extends beyond its scenic beauty, as it plays a pivotal role in the conservation of endangered species and the preservation of Sri Lanka's rich natural heritage map:"
+            body:"Yala (යාල/யாலா தேசிய பூங்கா) National Park is a mesmerizing haven for wildlife enthusiasts, situated in the southeastern region of Sri Lanka, bordering the Indian Ocean. Covering an expansive 979 square kilometers, Yala is the most visited and second-largest national park in the country.The park comprises five distinct blocks, with three open to the public. Notable among these are Palatupana (Block 1), Kumana National Park, also known as 'Yala East,' and Lunugamvehera National Park. Its strategic location, approximately 300 kilometers from Colombo, makes it easily accessible for nature lovers. Designated as a wildlife sanctuary in 1900, alongside Wilpattu, Yala has been a pioneer in wildlife conservation in Sri Lanka. Renowned for its incredible biodiversity, the park is a crucial habitat for Sri Lankan elephants, Sri Lankan leopards, and a plethora of aquatic bird species ,Explore the diverse landscapes within Yala National Park, from lush forests to arid plains, and witness the coexistence of a wide array of flora and fauna. The park's significance extends beyond its scenic beauty, as it plays a pivotal role in the conservation of endangered species and the preservation of Sri Lanka's rich natural heritage map:",
+            map:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.2390598342618!2d81.4235964740457!3d6.491384923537665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae683b969aec5c7%3A0x7e3aa2da875d927e!2sYala%20National%20Park%20Sri%20Lanka!5e0!3m2!1sen!2slk!4v1705477185395!5m2!1sen!2slk",
         },
-        tips:{
-            tip:"Book Safaris in Advance:",
-            tiptwo:"Visit During the Dry Season:",
-            tipthree:"Choose the Right Safari Duration:",
-            tipfour:"Follow Park Regulations",
-            tipfive:"Follow Park Regulations",
-            tipsix:"Wear Appropriate Clothing:",
-            tipseven:"Stay Hydrated and Carry Snacks",
-            tipeight:"Pack Binoculars and a Camera",
-            tipnine:"Arrive Early for Better Wildlife Sightings",
-        },
+       
         gallery:{
             topic:"",
-            image:"",
+            image:"/WDoS/images/yala-deer.jpeg",
             imagetwo:"",
-            imagethree:"",
+            
         },
 
         toggleEditor(block_id){
